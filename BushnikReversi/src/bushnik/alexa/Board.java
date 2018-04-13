a9abf29d840669a9ad98c63405fa3d1a8711785c
344870019 <344870019@C-11LFVR1.tdsb.on.ca>
2018-04-13 13:30:45 -0400

    Added to Driver, more to board

--- BushnikReversi/src/bushnik/alexa/Board.java
+++ BushnikReversi/src/bushnik/alexa/Board.java
@@ -1,5 +1,31 @@
 package bushnik.alexa;
 
 public class Board {
-
+	private String [][] board;
+	private int rows;
+	private int cols;
+	
+	public Board(int aRows, int aCols) {
+		board = new String[aRows][aCols];
+		rows = aRows;
+		cols = aCols;
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < cols; j++) {
+				System.out.print("A");
+			}
+		}
+	}
+	/*
+	String board [][] = new String [8][8];{
+	board[3][3] = "W";
+	board[4][3] = "B";
+	board[3][4] = "B";
+	board[4][4] = "W";
+	
+	for (int row = 0; row < 8; row ++){
+		for (int col = 0; col < 8; col++){
+			System.out.print("A");
+		}
+	}
+	*/
 }
--- BushnikReversi/src/bushnik/alexa/Driver.java
+++ BushnikReversi/src/bushnik/alexa/Driver.java
@@ -1,10 +1,25 @@
 package bushnik.alexa;
 
+import java.util.Scanner;
 public class Driver {
 
 	public static void main(String[] args) {
-		
-
+		String board [][] = new String [8][8];
+		displayBoard(board);
+		Scanner kb = new Scanner(System.in);
+		System.out.println("Which column do you want to place your piece?");
+		int col = kb.nextInt();
+		System.out.println("Which column do you want to place your piece?");
+		int row = kb.nextInt();
+		displayBoard(board);
+	}
+	public static void displayBoard (String[][] board){
+		for (int i = 0; i < board.length; i++) {
+			for (int j = 0; j < board[0].length; j++) {
+				System.out.printf("%3d", board[i][j]);
+			}
+		System.out.println();
+		}
 	}
 
 }
