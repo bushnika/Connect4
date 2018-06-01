448b673c46cc30aea9dd4d27ff3455d41403523d
bushnika <38222853+bushnika@users.noreply.github.com>
2018-06-01 15:20:26 -0400

    Update Turn.java

--- BushnikReversi/src/bushnik/alexa/Turn.java
+++ BushnikReversi/src/bushnik/alexa/Turn.java
@@ -2,19 +2,29 @@ package bushnik.alexa;
 
 public class Turn {
 	private int value; // the player.
-
+	/**
+	 * Creates a turn starting with a random player
+	 */
 	public Turn() {
 		this.value = (int) (Math.random() * 2.0d);
 	}
-
+	/**
+	 * Creates a turn starting with selected first player
+	 * @param v - value
+	 */
 	public Turn(int v) {
 		this.value = v;
 	}
-
+	/**
+	 * Returns the player whose turn it is
+	 * @return value of the player whose turn it is
+	 */
 	public int getTurn() {
 		return this.value;
 	}
-
+	/**
+	 * Change the turn to the other player
+	 */
 	public void change() {
 		this.value = (this.value + 1) % 2;
 	}
