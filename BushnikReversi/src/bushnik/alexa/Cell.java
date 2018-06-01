b5d0d591437ca0909ab00411fbaf0f83a5ff3625
bushnika <38222853+bushnika@users.noreply.github.com>
2018-06-01 15:19:28 -0400

    Update Cell.java

--- BushnikReversi/src/bushnik/alexa/Cell.java
+++ BushnikReversi/src/bushnik/alexa/Cell.java
@@ -4,54 +4,70 @@ public class Cell {
 
 	public static final char BLACK = 'B'; // displays 'B' for the black player.
 	public static final char WHITE = 'W'; // displays 'W' for the white player.
-	public static final char CANSELECT = '*'; // displays '*' for the possible
-												// moves available.
+	public static final char CANSELECT = '*'; // displays '*' for the possible moves available.
 
 	public boolean empty; // boolean if cell empty or not
 	public boolean canselect; // boolean if cell can be selected or not
 
 	public int value; // 0 = white, 1 = black, -1 = empty
 
-
+	/**
+	 * Class constructor. Cell is empty by default.
+	 */
 	public Cell() {
 		this.empty = true;
 		this.value = -1;
 	}
-
-
+	/**
+	 * Checks if cell is empty.
+	 * @return true when empty. False when not empty.
+	 */
 	public boolean isEmpty() {
 		return this.empty;
 	}
-
-
+	/**
+	 * Returns player's colour
+	 * @return the player's colour
+	 */
 	public int getPlayer() {
 		return this.value;
 	}
-
+	/**
+	 * Places a chip in the cell
+	 * @param player - the player chosen
+	 */
 	public void placeChip(int player) {
 		this.empty = false;
 		this.value = player;
 	}
-
+	/**
+	 * Changes the colour value of chip (0,1)
+	 */
 	public void changeChip() {
 		placeChip((value + 1) % 2);
 	}
-
-
+	/**
+	 * Makes cell possible to select/choose
+	 */
 	public void setSelect() {
 		this.canselect = true;
 	}
-
-
+	/**
+	 * Makes the cell selectable
+	 * @returns the cell that can be selected
+	 */
 	public boolean canSelect() {
 		return this.canselect;
 	}
-
+	/**
+	 * Makes the cell unselectable
+	 */
 	public void unselect() {
 		this.canselect = false;
 	}
-
-
+	/**
+	 * Displays the cell so that valid moves are *, black is B, and white is W
+	 */
 	public void display() {
 		if (this.isEmpty()) // if cell empty
 		{
